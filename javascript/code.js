7a0ecad19209e667dfad515621238848d1a69fd4
TANNER Disney <disney8358@students.cptc.edu>
2019-05-14 14:15:04 -0700

    Finished Basic ToDo ProtoType add/complete

---
+++ javascript/code.js
@@ -0,0 +1,81 @@
+var ToDoItem = (function () {
+    function ToDoItem() {
+    }
+    return ToDoItem;
+}());
+window.onload = function () {
+    var addBtn = document.querySelector("#create-item > button");
+    addBtn.onclick = processNewItem;
+    var readItemBtn = document.querySelector("#read-item > button");
+    readItemBtn.onclick = readItem;
+};
+var itemKey = "todo";
+function readItem() {
+    var item = JSON.parse(localStorage.getItem(itemKey));
+    alert(item.title);
+    alert(item.description);
+}
+function processNewItem() {
+    var item = getItemFromForm();
+    saveItem(item);
+    notifyUser();
+    clearForm();
+    displayToDo(item);
+}
+function displayToDo(item) {
+    var todoList = document.getElementById("todo-list");
+    var itemPara = document.createElement("p");
+    itemPara.innerText =
+        "Title: " + item.title + "\n" +
+            "Description: " + item.description + "\n" +
+            "Start Date: " + item.startDate + "\n" +
+            "Urgency: " + item.urgency;
+    itemPara.setAttribute("data-desc", item.description);
+    itemPara.onclick = toggleItemComplete;
+    todoList.appendChild(itemPara);
+}
+function toggleItemComplete() {
+    var currItem = this;
+    currItem.classList.toggle("completed");
+    var title = currItem.innerText;
+    var desc = currItem.getAttribute("data-desc");
+    alert("--Completed ToDo-- " + "\n" + title);
+}
+function clearForm() {
+    var textElements = document.querySelectorAll("input[type=text], textarea");
+    for (var i = 0; i < textElements.length; i++) {
+        textElements[i].value = "";
+    }
+    var isCompleteBox = document.querySelector("#is-complete");
+    isCompleteBox.checked = false;
+    var urgencyList = document.querySelector("#urgency");
+    urgencyList.selectedIndex = 0;
+}
+function notifyUser() {
+    alert("Todo Item was saved successfully.");
+    console.log("ToDo Item was saved to local storage successfully.");
+}
+function saveItem(item) {
+    var data = JSON.stringify(item);
+    console.log("Converting todoitem into JSON string...");
+    console.log(data);
+    if (typeof (Storage) != "undefined") {
+        localStorage.setItem(itemKey, data);
+    }
+}
+function getItemFromForm() {
+    var item = new ToDoItem();
+    item.title =
+        document.getElementById("title").value;
+    item.description =
+        document.getElementById("description").value;
+    var itemStartDate = document.getElementById("start-date").value;
+    item.startDate = new Date(itemStartDate);
+    var itemEndDate = document.getElementById("end-date").value;
+    item.endDate = new Date(itemEndDate);
+    item.isComplete =
+        document.getElementById("is-complete").checked;
+    var urgencyElem = document.getElementById("urgency");
+    item.urgency = urgencyElem.options[urgencyElem.selectedIndex].text;
+    return item;
+}
--- typescript/code.ts
+++ typescript/code.ts
@@ -18,3 +18,139 @@ class ToDoItem
     testItem.endDate = new Date("May 1, 2019");
     testItem.isComplete = true;
 */
+
+//When Add Item is clicked
+    //Get data off the page and wrap in ToDo object
+    //Notify user and clear form
+    //Save ToDo object
+
+    window.onload = function()
+    {
+        let addBtn = <HTMLButtonElement>
+            document.querySelector("#create-item > button");
+        addBtn.onclick = processNewItem;
+    
+        let readItemBtn = <HTMLElement>
+            document.querySelector("#read-item > button");
+        readItemBtn.onclick = readItem;
+    }
+    
+    const itemKey:string = "todo";
+    
+    function readItem()
+    {
+        //get item from storage
+        let item:ToDoItem = 
+            JSON.parse(localStorage.getItem(itemKey));
+    
+        alert(item.title);
+        alert(item.description);
+    }
+    
+    function processNewItem()
+    {
+        let item:ToDoItem = getItemFromForm();
+        saveItem(item);
+        notifyUser();
+        clearForm();
+        displayToDo(item);
+    }
+
+    function displayToDo(item:ToDoItem)
+    {
+        let todoList = document.getElementById("todo-list");
+        let itemPara = document.createElement("p");
+        itemPara.innerText = 
+            "Title: " + item.title + "\n" + 
+            "Description: " + item.description + "\n" + 
+            "Start Date: " + item.startDate + "\n" +
+            "Urgency: " + item.urgency;
+        itemPara.setAttribute("data-desc", item.description);
+        itemPara.onclick = toggleItemComplete;
+    
+        todoList.appendChild(itemPara);
+            
+    }
+
+    function toggleItemComplete()
+    {
+        let currItem:HTMLElement = this;
+        currItem.classList.toggle("completed");
+        let title = currItem.innerText;
+        let desc = currItem.getAttribute("data-desc");
+        alert("--Completed ToDo-- " + "\n" + title);
+    }
+    
+    function clearForm()
+    {
+        //We could alternatively, wrap all inputs in
+        //a <form> and reset the form
+    
+        //clear all textboxes and textarea
+        let textElements =
+            document.querySelectorAll("input[type=text], textarea");
+        for(let i = 0; i < textElements.length; i++)
+        {
+            (<HTMLInputElement>textElements[i]).value = "";
+        }
+    
+        //uncheck is complete
+        let isCompleteBox = <HTMLInputElement>
+                    document.querySelector("#is-complete");
+        isCompleteBox.checked = false;
+    
+        //reset select list
+        let urgencyList = <HTMLSelectElement>
+            document.querySelector("#urgency");
+        urgencyList.selectedIndex = 0;
+    }
+    
+    function notifyUser()
+    {
+        alert("Todo Item was saved successfully.");
+        console.log("ToDo Item was saved to local storage successfully.");
+    }
+    
+    function saveItem(item:ToDoItem):void
+    {
+    
+        let data:string = JSON.stringify(item);
+        console.log("Converting todoitem into JSON string...");
+        console.log(data);
+    
+        //ensure user can use localStorage
+        if(typeof(Storage) != "undefined")
+        {
+            localStorage.setItem(itemKey, data);
+        }
+    }
+    
+    /**
+     * Get all user input from Form
+     * and wrap it in a ToDoItem
+     */
+    function getItemFromForm():ToDoItem
+    {
+        let item = new ToDoItem();
+    
+        item.title = 
+            (<HTMLInputElement>document.getElementById("title")).value;
+        item.description = 
+            (<HTMLTextAreaElement>document.getElementById("description")).value;
+    
+        let itemStartDate:string = 
+            (<HTMLInputElement>document.getElementById("start-date")).value;
+        item.startDate = new Date(itemStartDate);
+    
+        let itemEndDate:string = 
+            (<HTMLInputElement>document.getElementById("end-date")).value;
+        item.endDate = new Date(itemEndDate);
+    
+        item.isComplete = 
+            (<HTMLInputElement>document.getElementById("is-complete")).checked;
+    
+        let urgencyElem = <HTMLSelectElement>document.getElementById("urgency");
+        item.urgency = urgencyElem.options[urgencyElem.selectedIndex].text;
+    
+        return item;
+    }
